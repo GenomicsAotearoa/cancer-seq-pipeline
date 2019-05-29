9261de5a8ec6dc5f078b8cdc16611459533b7c73
tirohia <ben@tirohia.net>
2019-05-29 04:34:21 +0000

    minor changes to file input

--- src/generateBpipes2.py
+++ src/generateBpipes2.py
@@ -11,27 +11,34 @@ def main(argv):
     
     parser = ArgumentParser()
     parser.add_argument( dest="comparisons", help="write report to FILE", metavar="comparisonsFile(.csv)")
-    parser.add_argument( dest="workflow", help="write report to FILE", metavar="workflow(.csv)")
+
+    # wrokflow generation needs to be developed
+    #parser.add_argument( dest="workflow", help="write report to FILE", metavar="workflow(.csv)")
 
     parser.add_argument("-q", "--quiet", action="store_false", dest="verbose", default=True, help="don't print status messages to stdout")
 
     args = parser.parse_args()
+    
+    # This needs to come from a config file rather than be hardcoded
+    dataDir="/nesi/project/uoa02461/data/raw/gastric"
 
     try:
         df = pd.read_csv (args.comparisons)
     except IOError:
         print("file " + filename + "doesn't exist or is malformed")
         # doesn't exist
-    try:
-        workflow = pd.read_csv (args.workflow)
-    except IOError:
-        print("file " + filename + "doesn't exist or is malformed")
+
+    # workflow generation needs to be developed
+    #try:
+    #    workflow = pd.read_csv (args.workflow)
+    #except IOError:
+    #    print("file " + filename + "doesn't exist or is malformed")
     
 
     currentPatient = ""
     
-
-    workflow = "run { [  control * [trim + align.using(type:'control') + removeDuplicates + removeSuplementary ],   samples *  [trim + align.using(type:'test') + removeDuplicates + removeSuplementary ]] + samples * [  pileUp  ] + samples * [annotation] }"
+    # workflow generation needs to go here.
+    workflow = "run { [  control * [trim + align.using(type:'control') + removeDuplicates + removeSuplementary],   samples *  [trim + align.using(type:'test') + removeDuplicates + removeSuplementary ]] + samples * [  pileUp  ] + samples * [annotation] }"
 
     for index, row in df.iterrows():
         patient = row["patient"]
@@ -46,6 +53,7 @@ def main(argv):
             # open the new one
             pipelineFile  = open("minimalPipe-"+ patient, "w")
             pipelineFile.write("load \"pipeline\" \n\n")
+            pipelineFile.write("$dataDir="+ dataDir)
             pipelineFile.write("control = [ " +patient + sampleNumber + " : [\"" + patient  + sampleNumber + "_1.fastq.gz\",\"" + patient  + sampleNumber + "_2.fastq.gz\"]\n]\n")
             pipelineFile.write("samples = [\n")
 
