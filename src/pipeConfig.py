5344a88835c431f3b01725b166f6dc5d42ca03fa
ben <ben@tirohia.net>
2019-07-01 22:53:56 +1200

    added config files for directories and started singularity transition

---
+++ src/pipeConfig.py
@@ -0,0 +1,9 @@
+baseDirectory = "/blue/project/cancer-seq-pipeline/"
+dataDirectory = "/blue/project/cancer-seq-pipeline/data/raw/wes/"
+intermediateDirectory = "/blue/project/cancer-seq-pipeline/data/intermediate/"
+
+bwaIndex= "/blue/project/cancer-seq-pipeline/data/references/hg19/ucsc.hg19.fasta"
+bwaIndexDir= "/blue/project/cancer-seq-pipeline/data/references/hg19/"
+
+
+
